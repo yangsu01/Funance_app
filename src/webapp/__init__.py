46b00d3438da0831e68f2ea81937d2e42a683433
yangsu01 <ssgc1101@gmail.com>
2024-02-23 20:11:57 -0800

    add scheduler

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_apscheduler import APScheduler
+from datetime import datetime #TODO delete later??
 
 db = SQLAlchemy()
 DB_NAME = "data.db"
 
 
+class Config:
+    SCHEDULER_API_ENABLED = True
+
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     app.config['SECRET_KEY'] = 'spooky secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
@@ -21,11 +28,37 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(sim, url_prefix='/')
 
-    from .data_models import User
+    # initiate scheduler
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+
+    # define jobs
+    def run_hourly():
+        from .portfolio_sim import update_prices, update_portfolio_value
+
+        with app.app_context():
+            update_prices()
+            update_portfolio_value()
+            print('Prices updated at ', datetime.now().strftime('%H:%M:%S')) #TODO temp for testing
+
+    def run_daily():
+        from .portfolio_sim import save_portfolio_value
+
+        with app.app_context():
+            save_portfolio_value()
+            print('Portfolio value saved at ', datetime.now().strftime('%H:%M:%S')) #TODO temp for testing
+
+    # add jobs
+    scheduler.add_job(id='Hourly task', func=run_hourly, trigger='interval', hours=1)
+    scheduler.add_job(id='Daily task', func=run_daily, trigger='cron', hour=17, minute=0)
+
+    scheduler.start()
     
-    # create db
+    # create db if not already created
     with app.app_context():
         db.create_all()
+
+    from .data_models import User
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
