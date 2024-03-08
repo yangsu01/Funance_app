53df295264b89dcb2191470174a5d8f0fd17d4b8
yangsu01 <ssgc1101@gmail.com>
2024-03-07 22:52:39 -0800

    update database structure

--- src/webapp/data_models.py
+++ src/webapp/data_models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    creation_date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    creation_date = db.Column(db.Date, nullable=False)
     # one to one
     portfolio = db.relationship('Portfolio', back_populates='user', uselist=False, cascade='all, delete-orphan')
 
@@ -20,9 +20,10 @@ class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
     available_cash = db.Column(db.Float, nullable=False)
-    creation_date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    creation_date = db.Column(db.Date, nullable=False)
     updated_value = db.Column(db.Float, nullable=False)
     updated_time = db.Column(db.DateTime(timezone=True), nullable=False)
+    last_close_value = db.Column(db.Float, nullable=False)
 
     # one to one
     user = db.relationship('User', back_populates='portfolio')
@@ -37,27 +38,26 @@ class Portfolio(db.Model):
 class Holdings(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'), nullable=False)
-    name = db.Column(db.String(150), nullable=False)
+    company_name = db.Column(db.String(150), nullable=False)
     ticker = db.Column(db.String(10), nullable=False)
-    shares = db.Column(db.Integer, nullable=False)
+    number_of_shares = db.Column(db.Integer, nullable=False)
     average_price = db.Column(db.Float, nullable=False)
     updated_price = db.Column(db.Float, nullable=False)
     currency = db.Column(db.String(5), nullable=False)
-    open_price = db.Column(db.Float, nullable=False)
-    open_price_date = db.Column(db.Date, nullable=False) #new column
+    opening_price = db.Column(db.Float, nullable=False)
 
 
 # transactions history
 class Transactions(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'), nullable=False)
-    date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    transaction_date = db.Column(db.DateTime(timezone=True), nullable=False)
     status = db.Column(db.String(10), nullable=False)
-    name = db.Column(db.String(150), nullable=False)
+    company_name = db.Column(db.String(150), nullable=False)
     ticker = db.Column(db.String(10), nullable=False)
     currency = db.Column(db.String(5), nullable=False)
-    shares = db.Column(db.Integer, nullable=False)
-    share_price = db.Column(db.Float, nullable=False)
+    number_of_shares = db.Column(db.Integer, nullable=False)
+    price_per_share = db.Column(db.Float, nullable=False)
     total_value = db.Column(db.Float, nullable=False)
 
 
@@ -65,7 +65,7 @@ class Transactions(db.Model):
 class History(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'), nullable=False)
-    date = db.Column(db.Date, nullable=False)
+    record_time = db.Column(db.DateTime(timezone=True), nullable=False)
     portfolio_value = db.Column(db.Float, nullable=False)
 
 
