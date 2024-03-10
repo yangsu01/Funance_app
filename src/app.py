6d2a2ad9d7452152c67e9286f12766a31d96fea0
yangsu01 <ssgc1101@gmail.com>
2024-03-09 21:00:48 -0800

    update port

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from webapp import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
